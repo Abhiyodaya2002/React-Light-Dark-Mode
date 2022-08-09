56ff8c11d4e0c6539ae0655011e7a55ccb2ac8a8
abhiyodaya <abhiyodayapandey@gmail.com>
2022-08-09 20:00:32 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React ,{useState} from "react";
+import Navbar from "./components/Navbar";
+import Alert from "./components/Alert";
+import Home from "./components/Home";
+import About from "./components/About";
+import {
+ // BrowserRouter as Router,   ---> in case any errors uncomment it.
+  Switch,
+  Route ,
+} from "react-router-dom";
+
 
 function App() {
+const [mode,setMode]=useState("light");
+const [alert,setAlert]=useState(null);
+const showAlert=(message,type)=>{
+  setAlert({
+    msg:message,
+    type:type
+  })
+
+  setTimeout(() => { //Alert will automatically be removed after 1.5 seconds
+    setAlert(null);
+  }, 1500);
+}
+const togglefunc=()=>{ //note: instead of "const togglefunc=()=> " we can write function togglefunc() 
+  if(mode==='dark')
+  {
+    document.body.style.backgroundColor="white";
+    setMode('light');
+    showAlert("Light mode has been enabled ","success");
+  }
+  else
+  {
+    document.body.style.backgroundColor="#042743";
+    setMode('dark');
+    showAlert("Dark mode has been enabled ","success");
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+     <Navbar title="React.js" mode={mode} toggle={togglefunc}/>
+     <div>
+       <Alert alert={alert}/>
+     </div>
+  
+     <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+     </Switch>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Home (){
+    return(
+        
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://source.unsplash.com/1600x600/?tech,gadgets" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="https://source.unsplash.com/1600x600/?software,programs" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="https://source.unsplash.com/1600x600/?computer,gadgets" className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+    )
+}
